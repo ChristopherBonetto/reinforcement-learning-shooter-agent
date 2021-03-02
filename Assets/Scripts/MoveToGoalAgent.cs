aa5a8e2c9f9c9c085e16c4b989110f42593077f3
Christopher.Bonetto <christopherbonetto@gmail.com>
2021-03-02 11:53:27 +0100

    part 1

---
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer terrainMeshRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-4f, +1f), 0, Random.Range(-4f,4f));
+        targetTransform.localPosition = new Vector3(Random.Range(2.4f, +4f), 0, Random.Range(-4f,4f));
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        float moveX = vectorAction[0];
+        float moveZ = vectorAction[1];
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            terrainMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            terrainMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+
+    }
+}
